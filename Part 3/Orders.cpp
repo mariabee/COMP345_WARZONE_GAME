7707e86468294be4f125df59d4acc982d9b5c341
mariabee <36023257+mariabee@users.noreply.github.com>
2022-01-24 16:30:56 -0500

    Made data members of orders pointers, implemented a destructor, copy constructor, default constructor to go with them.

--- Part 3/Orders.cpp
+++ Part 3/Orders.cpp
@@ -1,40 +1,64 @@
 #include "Orders.h"
 #include <list> 
+// BASE CLASS
 
+order::order()
+{
+    order_effect_ = nullptr;
+    order_type_ = nullptr; 
+}
+//COPY CONSTRUCTOR
+order::order(const order& other) 
+{
+    order_type_ = new string;
+    order_effect_ = new string;
+    *order_type_ = *other.order_type_;
+    *order_effect_ = *other.order_effect_; 
+}
+//DESTRUCTOR 
+order::~order()
+{
+    delete order_effect_;
+    delete order_type_; 
+}
 
 //ACCCESOR IMPLEMENTATION 
-string Order::getOrderType() { return orderType;; };
-string Order::getOrderEffect() { return orderEffect; };
+string order::get_order_type() const { return *order_type_; }
+string order::get_order_effect() const { return *order_effect_; }
 //MUTATOR IMPLEMENTATION 
-void Order::setOrderType(string order) { orderType = order; };
-void Order::setOrderEffect(string effect) { orderEffect = effect; };
+void order::set_order_type(string order) { order_type_ = &order; }
+void order::set_order_effect(string effect) { order_effect_ = &effect; }
 
 //STREAM INSERTION OPERATOR IMPLEMENTATION 
-ostream& operator << (ostream& strm, const Order& orderObj) {
+ostream& operator << (ostream& stream, const order& order_obj) {
 
-    return strm << orderObj.orderType << endl;
+    return stream << "Order Type : " << order_obj.get_order_type() << endl
+	<< "Order Effect : " << order_obj.get_order_effect() << endl; 
 }
 
 //SUBCLASS IMPLEMENTATION 
 
 //DEPLOY
-Deploy::Deploy() { setOrderType("Deploy"); }
+Deploy::Deploy()
+{
+	set_order_type("Deploy");
+}
 void Deploy::execute() {
     if (validate()) {
-        setOrderEffect("Troops have been deployed.");
+        set_order_effect("Troops have been deployed.");
     }
 }
 bool Deploy::validate() {
     //PLACEHOLDER
-        // if the territory blongs to the player
+        // if the territory belongs to the player
     return true;
 }
 
 //ADVANCE
-Advance::Advance() { setOrderType("Advance"); }
+Advance::Advance() { set_order_type("Advance"); }
 void Advance::execute() {
     if (validate()) {
-        setOrderEffect("Troops have advanced.*");
+        set_order_effect("Troops have advanced.*");
         /*
         * If the map territory does not belong to the player
         * Then an attack happens with other player
@@ -46,10 +70,10 @@ bool Advance::validate() {
 }
 
 //BOMB 
-Bomb::Bomb() { setOrderType("Bomb"); }
+Bomb::Bomb() { set_order_type("Bomb"); }
 void Bomb::execute() {
     if (validate()) {
-        setOrderEffect("Territory has been bombed.*");
+        set_order_effect("Territory has been bombed.*");
 
     }
 }
@@ -62,10 +86,10 @@ bool Bomb::validate() {
     }
 }
 //BLOCKADE
-Blockade::Blockade() { setOrderType("Blockade"); };
+Blockade::Blockade() { set_order_type("Blockade"); };
 void Blockade::execute() {
     if (validate()) {
-            setOrderEffect("Troops have tripled.\nThe territory is now neutral. *");
+        set_order_effect("Troops have tripled.\nThe territory is now neutral. *");
 
        }
 }
@@ -76,10 +100,10 @@ bool Blockade::validate() {
     //else false
 }
 //AIRLIFT 
-Airlift::Airlift() { setOrderType("Airlift"); }
+Airlift::Airlift() { set_order_type("Airlift"); }
 void Airlift::execute() {
     if (validate()) {
-        setOrderEffect("Troops have moved.*");
+        set_order_effect("Troops have moved.*");
         //if the target territory does not belongs to the player
         //then an attack happens
     }
@@ -89,10 +113,10 @@ bool Airlift::validate() {
     return true;
 }
 //NEGOTIATE 
-Negotiate::Negotiate() { setOrderType("Negotiate"); }
+Negotiate::Negotiate() { set_order_type("Negotiate"); }
 void Negotiate::execute() {
     if (validate()) {
-        setOrderEffect("Attacks have been prevented until the end of turn.*");
+        set_order_effect("Attacks have been prevented until the end of turn.*");
     }
 }
 bool Negotiate::validate() {
@@ -102,9 +126,9 @@ bool Negotiate::validate() {
 
 //TEMPORARY MAIN METHOD FOR TESTING 
 int main() {
-    cout << "THIS WORKS"; 
-    list<Order*> testList;
-    Deploy* d = new Deploy();
+    list<order*> testList;
+    Deploy d1; 
+    Deploy *d = &d1; //Alternative to using new ClassName()
     Advance* a = new Advance();
     Bomb* b = new Bomb();
     Blockade* bl = new Blockade();
@@ -116,6 +140,7 @@ int main() {
     testList.push_front(bl);
     testList.push_front(ai);
     testList.push_front(n);
+    cout << "THIS WORKS END";
 
 
 }

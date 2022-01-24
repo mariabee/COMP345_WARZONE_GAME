68dfb9612339bfd76b6a7d7bb713bff9ad56f160
there <there@DODO_TOTSI>
2022-01-23 22:45:09 -0500

    orders updated

--- Part 3/Orders.cpp
+++ Part 3/Orders.cpp
@@ -40,23 +40,117 @@ public:
 private:
     bool validate() {
         //PLACEHOLDER
+        // if the territory blongs to the player
         return true;
     }
 };
 class Advance : public Order {
+public:
+    //Default constructor 
+    Advance() { setOrderType("Advance"); }
+    //add MapTerritory* and Player* to the param
+    void execute() {
+        if (validate()) {
+            setOrderEffect("Troops have advanced.*");
+            /*
+            * If the map territory does not belong to the player
+            * Then an attack happens with other player
+            */
+        }
+    }
+private:
+    bool validate() {
+        //PLACEHOLDER
+        return true;
+    }
 };
 class Bomb : public Order {
+public:
+    //Default constructor 
+    Bomb() { setOrderType("Bomb"); }
+
+    void execute() {
+        if (validate()) {
+            setOrderEffect("Territory has been bombed.*");
+
+        }
+    }
+private:
+    bool validate() {
+        //PLACEHOLDER
+        // if the target territory is adjacent to the player's territory
+        return true;
+        //else return false
+    }
 };
 class Blockade : public Order {
+public:
+    //Default constructor 
+    Blockade() { setOrderType("Blockade"); }
+
+    void execute() {
+        if (validate()) {
+            setOrderEffect("Troops have tripled.\nThe territory is now neutral. *");
+
+        }
+    }
+private:
+    bool validate() {
+        //PLACEHOLDER
+        //if the territory belongs to the player
+        return true;
+        //else false
+    }
 };
 class Airlift : public Order {
+public:
+    //Default constructor 
+    Airlift() { setOrderType("Airlift"); }
+
+    void execute() {
+        if (validate()) {
+            setOrderEffect("Troops have moved.*");
+            //if the target territory does not belongs to the player
+            //then an attack happens
+        }
+    }
+private:
+    bool validate() {
+        //PLACEHOLDER
+        return true;
+    }
 };
 class Negotiate : public Order {
+public:
+    //Default constructor 
+    Negotiate() { setOrderType("Negotiate"); }
+
+    void execute() {
+        if (validate()) {
+            setOrderEffect("Attacks have been prevented until the end of turn.*");
+        }
+    }
+private:
+    bool validate() {
+        //PLACEHOLDER
+        return true;
+    }
 };
 
 int main() {
     list<Order*> testList;
     Deploy* d = new Deploy();
+    Advance* a = new Advance();
+    Bomb* b = new Bomb();
+    Blockade* bl = new Blockade();
+    Airlift* ai = new Airlift();
+    Negotiate* n = new Negotiate();
     testList.push_front(d);
+    testList.push_front(a);
+    testList.push_front(b);
+    testList.push_front(bl);
+    testList.push_front(ai);
+    testList.push_front(n);
+
 
 }

512b0c2fd4a8f8499a2ea9ee53ee06945d0d8573
mariabee <36023257+mariabee@users.noreply.github.com>
2022-02-13 20:15:05 -0500

    added copy constructor and copy assignment to ordersList class

--- Part 3/Orders.cpp
+++ Part 3/Orders.cpp
@@ -518,12 +518,24 @@ OrdersList::OrdersList(){
 OrdersList::OrdersList(const OrdersList &other) {
     list = new vector<order *>();
     ptr = new vector<order *>::iterator();
-    for (order *o : *other.list) {
+    for (order *o : *other.getList()) {
         add(o);
     }
 }
+OrdersList &OrdersList:: operator=(const OrdersList& o) {
+    if (this == &o) return *this;
+    for (order *o : *list) {
+        delete o;
+    }
+    list->clear();
+    for (order *o : *o.getList()) {
+        add(o);
+    }
+    return *this;
+}
 
 OrdersList::~OrdersList() {
+    //Deleting OrdersList also currently deletes the orders it references.
     for (order *o : *list) {
         delete o;
     }
@@ -533,6 +545,8 @@ OrdersList::~OrdersList() {
     list = nullptr;
     ptr = nullptr;
 }
+vector<order *> *OrdersList::getList() const { return list; }
+
 #pragma endregion OrderList
 
 

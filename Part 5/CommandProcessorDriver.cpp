3e51ed08cbac1e59993a983c597a336c12a986ad
ccassidy <ccassidy020@gmail.com>
2022-03-29 09:27:42 -0400

    fixed stuff

--- Part 5/CommandProcessorDriver.cpp
+++ Part 5/CommandProcessorDriver.cpp
@@ -1,17 +1,16 @@
-#include "GameEngine.h"
-#include "CommandProcessor.h"
-#include <iostream>
+// #include "GameEngine.h"
+// #include "CommandProcessor.h"
+// #include <iostream>
 
-using namespace std;
+// using namespace std;
 
-int main()
-{
-    cout<<"Hello World";
-    GameEngine *ge = new GameEngine();
-    CommandProcessor* cp = new CommandProcessor(ge);
-    cp->getCommand();
-    ge->play();
-    delete ge;
+// int main()
+// {
+//     GameEngine *ge = new GameEngine();
+//     CommandProcessor* cp = new CommandProcessor(ge);
+//     cp->getCommand();
+//     ge->play();
+//     delete ge;
 
-    return 0;
-}
+//     return 0;
+// }
